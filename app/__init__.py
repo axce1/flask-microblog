577df0b23bb0972600bef3c6b43419f4ea91d1b0
evgen <evgen@funtoo>
2014-03-05 16:06:55 +0600

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+
+from config import basedir
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+from app import views, models
+
