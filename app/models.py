41b0be376c6e5233d1af6b0afde220e0c9d3b8cb
evgen <evgen@funtoo>
2014-03-11 16:18:57 +0600

    add tests

--- app/models.py
+++ app/models.py
@@ -69,6 +69,9 @@ class User(db.Model):
             self.followed.remove(user)
             return self
 
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
